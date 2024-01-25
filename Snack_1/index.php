0ed64505eaa940f543609d5ea51c7cc487d2fa89
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2024-01-25 13:06:03 +0100

    first commit

---
+++ Snack_1/index.php
@@ -0,0 +1,53 @@
+<?php 
+    $giornata = [
+        [
+            'casa' => 'Trento',
+            'ospite' => 'Brescia',
+            'punti casa' => 69,
+            'punti ospite' => 93
+        ],
+        [
+            'casa' => 'Brindisi',
+            'ospite' => 'Napoli',
+            'punti casa' => 77,
+            'punti ospite' => 80
+        ],
+        [
+            'casa' => 'Derthona',
+            'ospite' => 'Venezia',
+            'punti casa' => 75,
+            'punti ospite' => 69
+        ],
+        [
+            'casa' => 'VL Pesaro',
+            'ospite' => 'Olimpia Milano',
+            'punti casa' => 65,
+            'punti ospite' => 85
+        ],
+        [
+            'casa' => 'Pistoia',
+            'ospite' => 'Scafati',
+            'punti casa' => 78,
+            'punti ospite' => 71
+        ],
+        [
+            'casa' => 'Varese',
+            'ospite' => 'Virtus Bologna',
+            'punti casa' => 69,
+            'punti ospite' => 81
+        ],
+        [
+            'casa' => 'Reggiana',
+            'ospite' => 'Cremona',
+            'punti casa' => 78,
+            'punti ospite' => 71
+        ],
+        [
+            'casa' => 'Universo Treviso',
+            'ospite' => 'Dinamo Sassari',
+            'punti casa' => 77,
+            'punti ospite' => 70
+        ],
+    ]
+
+?>
